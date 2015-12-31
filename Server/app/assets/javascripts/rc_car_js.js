c355a1ac36718a8710552ccf36d95259fe6c40a4
RoyaLTigeR <royal_tiger_rk@hotmail.com>
2015-12-30 17:04:54 -0800

    a lot of shit

---
+++ Server/app/assets/javascripts/rc_car_js.js
@@ -0,0 +1,41 @@
+function camera_down(camera_down) {
+
+}
+
+function camera_up(camera_up) {
+
+}
+
+function left(left) {
+
+}
+
+function right(right) {
+
+}
+
+function reverse(reverse) {
+	//alert("background color = " + $('#button_reverse').css("background-color"));
+
+	if ($("#button_reverse").css("background-color") == "rgb(255, 0, 0)") {
+		$("#button_reverse").css("background-color","#8A0404");
+	} else {
+		$("#button_reverse").css("background-color","#FF0000");
+	}
+}
+
+function accelerate(accelerate) {
+	if ($("#button_accelerate").css("background-color") == "rgb(1, 223, 1)") {
+		$("#button_accelerate").css("background-color","#085F08");
+	} else {
+		$("#button_accelerate").css("background-color","#01DF01");
+	}
+}
+
+function microphone(microphone) {
+	alert("microphone " + microphone.checked);
+}
+
+function lights(lights) {
+	alert("lights " + lights.checked);
+}
