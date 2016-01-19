72b6fd60eafdec7832d1457c914aaea39a131102
RoyaLTigeR <royal_tiger_rk@hotmail.com>
2016-01-19 14:05:39 -0800

    stuff

--- Server/app/assets/javascripts/rc_car_js.js
+++ Server/app/assets/javascripts/rc_car_js.js
@@ -2,48 +2,48 @@
 
 //alert("background color = " + $('#button_reverse').css("background-color"));
 
-function video_play(video) {
-	video.play();
-}
+// function video_play(video) {
+// 	video.play();
+// }
 
-function camera_down(camera_down) {
+// function camera_down(camera_down) {
 
-}
+// }
 
-function camera_up(camera_up) {
+// function camera_up(camera_up) {
 
-}
+// }
 
-function left(left) {
+// function left(left) {
 
-}
+// }
 
-function right(right) {
+// function right(right) {
 
-}
+// }
 
-function reverse(reverse) {
-	//alert("background color = " + $('#button_reverse').css("background-color"));
+// function reverse(reverse) {
+// 	//alert("background color = " + $('#button_reverse').css("background-color"));
 
-	if ($("#button_reverse").css("background-color") == "rgb(255, 0, 0)") {
-		$("#button_reverse").css("background-color","#8A0404");
-	} else {
-		$("#button_reverse").css("background-color","#FF0000");
-	}
-}
+// 	if ($("#button_reverse").css("background-color") == "rgb(255, 0, 0)") {
+// 		$("#button_reverse").css("background-color","#8A0404");
+// 	} else {
+// 		$("#button_reverse").css("background-color","#FF0000");
+// 	}
+// }
 
-function accelerate(accelerate) {
-	if ($("#button_accelerate").css("background-color") == "rgb(1, 223, 1)") {
-		$("#button_accelerate").css("background-color","#085F08");
-	} else {
-		$("#button_accelerate").css("background-color","#01DF01");
-	}
-}
+// function accelerate(accelerate) {
+// 	if ($("#button_accelerate").css("background-color") == "rgb(1, 223, 1)") {
+// 		$("#button_accelerate").css("background-color","#085F08");
+// 	} else {
+// 		$("#button_accelerate").css("background-color","#01DF01");
+// 	}
+// }
 
-function microphone(microphone) {
-	alert("microphone " + microphone.checked);
-}
+// function microphone(microphone) {
+// 	alert("microphone " + microphone.checked);
+// }
 
-function lights(lights) {
-	alert("lights " + lights.checked);
-}
+// function lights(lights) {
+// 	alert("lights " + lights.checked);
+// }
