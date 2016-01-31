ed16bbc97e16ee4d38f35de05bfacdf7302cabcd
RoyaLTigeR <royal_tiger_rk@hotmail.com>
2016-01-30 23:13:17 -0800

    rc car

---
+++ RcCar/RC-Car/html/rc_car.js
@@ -0,0 +1,51 @@
+webiopi().ready(function() {
+	webiopi().refreshGPIO(true);
+});
+
+function top_left(button) {
+	webiopi().callMacro("top_left");
+}
+
+function top_middle(button) {
+	webiopi().callMacro("top_middle");
+}
+
+function top_right(button) {
+	webiopi().callMacro("top_right");
+}
+
+function middle_left(button) {
+	webiopi().callMacro("middle_left");
+}
+
+function middle_middle(button) {
+	webiopi().callMacro("middle_middle");
+}
+
+function middle_right(button) {
+	webiopi().callMacro("middle_right");
+}
+
+function bottom_left(button) {
+	webiopi().callMacro("bottom_left");
+}
+
+function bottom_middle(button) {
+	webiopi().callMacro("bottom_middle");
+}
+
+function bottom_right(button) {
+	webiopi().callMacro("bottom_right");
+}
+
+function microphone(microphone) {
+	
+}
+
+function lights(lights) {
+	if (lights.checked) {
+		webiopi().callMacro("lightsOn");
+	} else {
+		webiopi().callMacro("lightsOff");
+	}
+}
---
+++ RcCar/RC-Car/python/script.py
@@ -0,0 +1,95 @@
+import webiopi
+
+GPIO = webiopi.GPIO
+
+PIN_LIGHT = 17
+
+PIN_RIGHT_FORWARD = 27
+PIN_RIGHT_BACKWARD = 22
+
+PIN_LEFT_FORWARD = 23
+PIN_LEFT_BACKWARD = 24
+
+def setup():
+	GPIO.setFunction(PIN_LIGHT, GPIO.OUT)
+	
+	GPIO.setFunction(PIN_RIGHT_FORWARD, GPIO.OUT)
+	GPIO.setFunction(PIN_RIGHT_BACKWARD, GPIO.OUT)
+	
+	GPIO.setFunction(PIN_LEFT_FORWARD, GPIO.OUT)
+	GPIO.setFunction(PIN_LEFT_BACKWARD, GPIO.OUT)
+
+#def loop():
+	# Nothing for now.
+
+def destroy():
+	lightsOff()
+
+@webiopi.macro
+def top_left():
+	stop()
+	GPIO.digitalWrite(PIN_RIGHT_FORWARD, GPIO.HIGH)
+
+@webiopi.macro
+def top_middle():
+	stop()
+	GPIO.digitalWrite(PIN_RIGHT_FORWARD, GPIO.HIGH)
+	GPIO.digitalWrite(PIN_LEFT_FORWARD, GPIO.HIGH)
+
+@webiopi.macro
+def top_right():
+	stop()
+	GPIO.digitalWrite(PIN_LEFT_FORWARD, GPIO.HIGH)
+
+@webiopi.macro
+def middle_left():
+	stop()
+	GPIO.digitalWrite(PIN_RIGHT_FORWARD, GPIO.HIGH)
+	GPIO.digitalWrite(PIN_LEFT_BACKWARD, GPIO.HIGH)
+
+@webiopi.macro
+def middle_middle():
+	stop()
+
+@webiopi.macro
+def middle_right():
+	stop()
+	GPIO.digitalWrite(PIN_RIGHT_BACKWARD, GPIO.HIGH)
+	GPIO.digitalWrite(PIN_LEFT_FORWARD, GPIO.HIGH)
+
+@webiopi.macro
+def bottom_left():
+	stop()
+	GPIO.digitalWrite(PIN_RIGHT_BACKWARD, GPIO.HIGH)
+
+@webiopi.macro
+def bottom_middle():
+	stop()
+	GPIO.digitalWrite(PIN_RIGHT_BACKWARD, GPIO.HIGH)
+	GPIO.digitalWrite(PIN_LEFT_BACKWARD, GPIO.HIGH)
+
+@webiopi.macro
+def bottom_right():
+	stop()
+	GPIO.digitalWrite(PIN_LEFT_BACKWARD, GPIO.HIGH)
+
+@webiopi.macro
+def stop():
+	GPIO.digitalWrite(PIN_RIGHT_FORWARD, GPIO.LOW)
+	GPIO.digitalWrite(PIN_RIGHT_BACKWARD, GPIO.LOW)
+	GPIO.digitalWrite(PIN_LEFT_FORWARD, GPIO.LOW)
+	GPIO.digitalWrite(PIN_LEFT_BACKWARD, GPIO.LOW)
+
+@webiopi.macro
+def lightsOn():
+	GPIO.digitalWrite(PIN_LIGHT, GPIO.HIGH)
+
+@webiopi.macro
+def lightsOff():
+	GPIO.digitalWrite(PIN_LIGHT, GPIO.LOW)
+
+@webiopi.macro
+def getLights():
+	if (GPIO.digitalRead(PIN_LIGHT) == GPIO.LOW):
+		return false
+	return true
